c2682063b06e40c6ae2de2121bf8b1ebe392cf95
Rodrigo Rojas <rrojas@clemson.edu>
2018-04-05 09:42:03 -0400

    update

--- sortWashers.m
+++ sortWashers.m
@@ -2,15 +2,16 @@ function sortWashers(initial, final)
     
     for c1 = 1:numel(final)
         color = final{c1, 2};
-        if (color ~= '')
+        if ((strcmp(color, '')) == 0)
             for c2 = 1:numel(initial)
                 color2 = initial{c2}{2};
-                if ((strcmp(color, colo2)) == 1)
-                    weight = final{c1, 3)
-                    fprintf('Here\n');
-                    if (weight ~= '')
+                if ((strcmp(color, color2)) == 1)
+                    weight = final(c1, 3);
+                    weight = str2double(weight);
+                    if ((strcmp(weight, '')) == 0)
+                        fprintf('Here\n');
                         %Calculate weight of picked up washer
-                        %weightPicked = function
+                        %weightPicked = loadCellFunction
                         if ((weightPicked < 10) && (weight < 10))
                             %Level one picked up and moved
                             %Erase value from original 
