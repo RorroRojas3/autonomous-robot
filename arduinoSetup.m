3483293ba45d366595f0108b408929557f0acee2
Rodrigo Rojas <rrojas@clemson.edu>
2018-04-09 10:46:19 -0400

    update

--- arduinoSetup.m
+++ arduinoSetup.m
@@ -1,11 +1,15 @@
-function s = arduinoSetup()
+function [s1, s2] = arduinoSetup()
 
     %MATLAB recognizes and initializes ARDUINOUNO
     a = arduino;
 
     %Sets ARDUINO pins and maximum and minimum traveling or Servo
-    s = servo(a, 'D10', 'MaxPulseDuration', 2.55e-3, 'MinPulseDuration', 6.4e-4);
-
+    s1 = servo(a, 'D10', 'MaxPulseDuration', 2.55e-3, 'MinPulseDuration', 6.4e-4);
+    s2 = servo(a, 'D9', 'MaxPulseDuration', 2.3e-3, 'MinPulseDuration', 2.55e-3);
+    
     %Moves arm to the back 
-    writePosition(s, 1);
+    writePosition(s1, 1);
+    
+    %Moves arm up to initial position
+    %writePosition(s2, 1);
 end
--- sortWashers.m
+++ sortWashers.m
@@ -1,29 +1,100 @@
 function sortWashers(initial, final, tg)
-    servo = arduinoSetup();
+
+    % Sets up the Arm to initial position
+    [s1, s2] = arduinoSetup();
+    
     for c1 = 1:10
         color = final{c1, 2};
+        %Checks if the user input a color in certain degree
         if ((strcmp(color, '')) == 0)
             for c2 = 1:10
                 color2 = initial{c2}{2};
+                %Compares the initial color and the color input by user
                 if ((strcmp(color, color2)) == 1)
                     weight = final(c1, 3);
                     weight = str2double(weight);
+                    %Checks that the user has input a weight of the washer
+                    %desired to be moved 
                     if ((strcmp(weight, '')) == 0)
-                        fprintf('Here\n');
                         %Degree of the Washer to be weighted
                         degree = initial{c2}{1};
+                        
                         %Rotates motor to the corresponding degree
                         tg.setparam(tg.getparamid('Degree','Value'), degree);
-                        writePosition(servo, 0);
+                        
+                        %Moves arm to the board
+                        writePosition(s1, 0);
+                        
+                        %Moves arm up and down
+                        
                         %Calculate weight of picked up washer
                         %weightPicked = loadCellFunction
+                        
+                        
                         pause(3);
+                        
+                        
                         degree = 0;
                         %writePosition(servo, 1);
                         tg.setparam(tg.getparamid('Degree','Value'), degree);
                         %pause(3);
 %                         if ((weightPicked < 10) && (weight < 10))
-%                             %Level one picked up and moved
+                              desiredDegree = final{c1, 1};
+                            
+                              %Looks for the location on the desired
+                              %degree on the initial Table
+                              for c3 = 1:10
+                                if (initial{c2, 1} == desiredDegree)
+                                    colorOnDesiredDegree = initial{c3, 2};
+                                    break;
+                                end
+                              end
+                              
+%                             %Spot is empty
+                              if (strcmp(initial{c3, 2}, '') == 1)
+                                  %Pick up washer again
+                                  
+                                  %Go to empty spot
+                                  tg.setparam(tg.getparamid('Degree', 'Value'), desiredDegree);
+                                  pause(2);
+                                  
+                                  %Drop the washer
+                                  
+                                  
+                                  %Delete the position of washer in initial
+                                  %Table
+                                  initial{c3, 2} = '';
+                                  
+                              %Spot is filled
+                              else
+                                  %Search for an empty spot
+                                  for c4 = 1:10
+                                      if (strcmp(initial{c4, 2}, '') == 1)
+                                          break;
+                                      end
+                                  end
+                                  emptyDegree = initial{c4, 1};
+                                  
+                                  %Go to filled spot
+                                  tg.setparam(tg.getparamid('Degree', 'Value'), desiredDegree);
+                                  
+                                  %Pick up washer
+                                  
+                                  %Move washer to empty spot 
+                                  tg.setparam(tg.getparamid('Degree', 'Value'), emptyDegree);
+                                  
+                                  %Drop the washer
+                                  
+                                  %Modify initial table 
+                                  initial{c4, 2} = colorOnDesiredDegree;
+                                  
+                                  %Go to Washer 
+                                  tg.setparam(tg.getparamid('Degree', 'Value'), desiredDegree);
+                                  
+                                  
+                              end
+                                  
+                        
 %                             %Erase value from original 
 %                         elseif ((weightPicked > 10) && (weightPicked  < 15) && (weight > 10) && (weight < 15))
 %                             %Level two picked and moved
